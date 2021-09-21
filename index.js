b4e7d5d909380f0a03030363852c6d8927075137
michael-lugashi <89575620+michael-lugashi@users.noreply.github.com>
2021-09-21 20:21:30 +0300

    Sending and Receiving request
    I added a click event and made it so I can send and receive to and from the server.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 'use strict'
+async function positiveOrNegative(event) {
+    let text = document.getElementsByName('text')[0].value;
+    let response = await fetch(`https://sentim-api.herokuapp.com/api/v1/`, {
+        method: "POST",
+
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            text
+        })
+    })
+    let data = await response.json()
+    console.log(data);
+}
+sendToAPI.onclick = positiveOrNegative;

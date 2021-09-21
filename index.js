4f45d54bf6fbfbd2e176868b6b201006fff19d3f
michael-lugashi <89575620+michael-lugashi@users.noreply.github.com>
2021-09-21 21:10:55 +0300

    Displayed Result and Added Loading animation
    I displayed the result from the server and added a loading animation.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
 async function positiveOrNegative(event) {
+    result.textContent = ''
+    result.classList.add('loading')
     let text = document.getElementsByName('text')[0].value;
     let response = await fetch(`https://sentim-api.herokuapp.com/api/v1/`, {
         method: "POST",
@@ -14,5 +16,11 @@ async function positiveOrNegative(event) {
     })
     let data = await response.json()
     console.log(data);
+    let { result: { polarity, type } } = data
+    console.log(polarity)
+    console.log(type)
+    result.classList.remove('loading')
+    result.textContent = `${polarity} ${type}`
+
 }
 sendToAPI.onclick = positiveOrNegative;
